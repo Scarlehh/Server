799fb0369c81ca3ccb6c841340c95c8e0409462d
Scarlett <gourleys@tcd.ie>
2016-03-29 16:42:36 +0100

    Beginning of handler

--- ChatServer.py
+++ ChatServer.py
@@ -3,16 +3,20 @@ from Server import Server
 class ChatServer(Server):
 	def __init(self, host, port, sockBuffer=1024):
 		super(Server, self, host, port, sockBuffer).__init__()
-
-	def handler(self):
-		self.startServer()
-		
+	
+	def handler(self, conn, address, clients):
+		command = conn.recv(self.sockBuffer)
+		command = command.strip()
+		if command == 'join':
+			self.onReceipt(conn, address, clients)
+		else:
+			print "Didn't connect"
 		
 def main():
 	HOST = ''                 # Symbolic name meaning all available interfaces
 	PORT = 50010              # Arbitrary non-privileged port
 
 	server = ChatServer(HOST, PORT)
-	server.handler()
+	server.startServer()
 	
 if __name__ == "__main__": main()
